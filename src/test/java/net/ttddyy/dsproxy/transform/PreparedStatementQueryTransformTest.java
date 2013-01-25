b022d1bdd8a5b60fbf7ef33274fcf226c767d77f
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2013-01-24 16:54:38 -0800

    query transformer unittest for Statement and PreparedStatement

---
+++ src/test/java/net/ttddyy/dsproxy/transform/PreparedStatementQueryTransformTest.java
@@ -0,0 +1,182 @@
+package net.ttddyy.dsproxy.transform;
+
+import net.ttddyy.dsproxy.TestUtils;
+import net.ttddyy.dsproxy.listener.QueryExecutionListener;
+import net.ttddyy.dsproxy.proxy.InterceptorHolder;
+import net.ttddyy.dsproxy.proxy.JdkJdbcProxyFactory;
+import org.hsqldb.jdbc.jdbcDataSource;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author Tadaya Tsuyukubo
+ */
+public class PreparedStatementQueryTransformTest {
+    private DataSource rawDatasource;
+    private List<String> interceptedQueries = new ArrayList<String>();
+
+    @BeforeMethod
+    public void setup() throws Exception {
+        // real datasource
+        org.hsqldb.jdbc.jdbcDataSource rawDataSource = new jdbcDataSource();
+        rawDataSource.setDatabase("jdbc:hsqldb:mem:aname");
+        rawDataSource.setUser("sa");
+        this.rawDatasource = rawDataSource;
+
+        // populate datasource
+        Statement statement = rawDataSource.getConnection().createStatement();
+        statement.addBatch("create table foo ( id integer primary key, name varchar(10) );");
+        statement.addBatch("create table bar ( id integer primary key, name varchar(10) );");
+        statement.addBatch("insert into foo ( id, name )values (1, 'foo');");
+        statement.addBatch("insert into bar ( id, name )values (100, 'bar');");
+        statement.executeBatch();
+    }
+
+
+    @AfterMethod
+    public void teardown() throws Exception {
+        interceptedQueries.clear();
+        TestUtils.shutdown(rawDatasource);
+    }
+
+    private Connection getProxyConnectionForSelect() throws Exception {
+        return getProxyConnection(true);
+    }
+
+    private Connection getProxyConnectionForUpdate() throws Exception {
+        return getProxyConnection(false);
+    }
+
+    private Connection getProxyConnection(final boolean isSelect) throws Exception {
+
+        QueryTransformer transformer = new QueryTransformer() {
+            public String transformQuery(String dataSourceName, String query) {
+                interceptedQueries.add(query);
+                return isSelect ? "SELECT * FROM bar" : "UPDATE bar SET name = ?";
+            }
+        };
+
+        QueryExecutionListener queryListener = mock(QueryExecutionListener.class);
+        InterceptorHolder interceptorHolder = new InterceptorHolder(queryListener, transformer);
+
+        return new JdkJdbcProxyFactory().createConnection(rawDatasource.getConnection(), interceptorHolder);
+    }
+
+    @Test
+    public void testExecuteWithSelect() throws Exception {
+        PreparedStatement ps = getProxyConnectionForSelect().prepareStatement("SELECT name FROM foo");
+        boolean result = ps.execute();
+        assertThat(result, is(true));
+
+        // verify intercepted query
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries, hasItem("SELECT name FROM foo"));
+    }
+
+    @Test
+    public void testExecuteWithUpdate() throws Exception {
+        PreparedStatement ps = getProxyConnectionForUpdate().prepareStatement("UPDATE foo SET name = ?");
+        ps.setString(1, "FOO");
+        boolean result = ps.execute();
+        assertThat(result, is(false));
+
+        // verify intercepted query
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries, hasItem("UPDATE foo SET name = ?"));
+
+        // verify bar is updated instead of foo
+        ResultSet rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM foo");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("foo"));
+        rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM bar");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("FOO"));
+    }
+
+    @Test
+    public void testExecuteQuery() throws Exception {
+        PreparedStatement ps = getProxyConnectionForSelect().prepareStatement("SELECT name FROM foo");
+        ResultSet resultSet = ps.executeQuery();
+        assertThat(resultSet.next(), is(true));
+        assertThat(resultSet.getInt("id"), is(100));
+        assertThat(resultSet.getString("name"), is("bar"));
+
+        // verify intercepted query
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries, hasItem("SELECT name FROM foo"));
+    }
+
+    @Test
+    public void testExecuteUpdate() throws Exception {
+        PreparedStatement ps = getProxyConnectionForUpdate().prepareStatement("UPDATE foo SET name = ?");
+        ps.setString(1, "FOO");
+        int count = ps.executeUpdate();
+        assertThat(count, is(1));
+
+        // verify intercepted query
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries, hasItem("UPDATE foo SET name = ?"));
+
+        // verify bar is updated instead of foo
+        ResultSet rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM foo");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("foo"));
+        rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM bar");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("FOO"));
+    }
+
+    @Test
+    public void testExecuteBatch() throws Exception {
+        PreparedStatement ps = getProxyConnectionForUpdate().prepareStatement("UPDATE foo SET name = ?");
+        ps.setString(1, "FOO1");
+        ps.addBatch();
+        ps.setString(1, "FOO2");
+        ps.addBatch();
+        int[] result = ps.executeBatch();
+        assertThat(result.length, is(2));
+        assertThat(result[0], is(1));
+        assertThat(result[1], is(1));
+
+        // verify intercepted query
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries, hasItem("UPDATE foo SET name = ?"));
+
+        // verify bar is updated instead of foo
+        ResultSet rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM foo");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("foo"));
+        rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM bar");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("FOO2"));
+    }
+
+    @Test
+    public void testClearBatch() throws Exception {
+        PreparedStatement ps = getProxyConnectionForUpdate().prepareStatement("UPDATE foo SET name = ?");
+        ps.clearBatch();
+        int[] result = ps.executeBatch();
+
+        assertThat(result.length, is(0));
+
+        // even though batch is canceled, interceptor was called once.
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries, hasItem("UPDATE foo SET name = ?"));
+    }
+
+}
---
+++ src/test/java/net/ttddyy/dsproxy/transform/StatementQueryTransformTest.java
@@ -0,0 +1,187 @@
+package net.ttddyy.dsproxy.transform;
+
+import net.ttddyy.dsproxy.TestUtils;
+import net.ttddyy.dsproxy.listener.QueryExecutionListener;
+import net.ttddyy.dsproxy.proxy.InterceptorHolder;
+import net.ttddyy.dsproxy.proxy.JdkJdbcProxyFactory;
+import org.hsqldb.jdbc.jdbcDataSource;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author Tadaya Tsuyukubo
+ */
+public class StatementQueryTransformTest {
+
+    private DataSource rawDatasource;
+    private List<String> interceptedQueries = new ArrayList<String>();
+
+    @BeforeMethod
+    public void setup() throws Exception {
+        // real datasource
+        org.hsqldb.jdbc.jdbcDataSource rawDataSource = new jdbcDataSource();
+        rawDataSource.setDatabase("jdbc:hsqldb:mem:aname");
+        rawDataSource.setUser("sa");
+        this.rawDatasource = rawDataSource;
+
+        // populate datasource
+        Statement stat = rawDataSource.getConnection().createStatement();
+        stat.addBatch("create table foo ( id integer primary key, name varchar(10) );");
+        stat.addBatch("create table bar ( id integer primary key, name varchar(10) );");
+        stat.addBatch("insert into foo ( id, name )values (1, 'foo');");
+        stat.addBatch("insert into bar ( id, name )values (100, 'bar');");
+        stat.executeBatch();
+    }
+
+
+    @AfterMethod
+    public void teardown() throws Exception {
+        TestUtils.shutdown(rawDatasource);
+        interceptedQueries.clear();
+    }
+
+
+    private Connection getProxyConnectionForSelect() throws Exception {
+        return getProxyConnection(true);
+    }
+
+    private Connection getProxyConnectionForUpdate() throws Exception {
+        return getProxyConnection(false);
+    }
+
+    private Connection getProxyConnection(final boolean isSelect) throws Exception {
+
+        QueryTransformer transformer = new QueryTransformer() {
+            public String transformQuery(String dataSourceName, String query) {
+                interceptedQueries.add(query);
+                return isSelect ? "SELECT * FROM bar" : "UPDATE bar SET name = 'BAR'";
+            }
+        };
+
+        QueryExecutionListener queryListener = mock(QueryExecutionListener.class);
+        InterceptorHolder interceptorHolder = new InterceptorHolder(queryListener, transformer);
+
+        return new JdkJdbcProxyFactory().createConnection(rawDatasource.getConnection(), interceptorHolder);
+    }
+
+
+    @Test
+    public void testExecuteWithSelect() throws Exception {
+        Statement stat = getProxyConnectionForSelect().createStatement();
+        boolean result = stat.execute("SELECT name FROM foo");
+        assertThat(result, is(true));
+
+        // verify intercepted query
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries, hasItem("SELECT name FROM foo"));
+    }
+
+    @Test
+    public void testExecuteWithUpdate() throws Exception {
+        // stmt.execute() with update statement. (expect false)
+        Statement stat = getProxyConnectionForUpdate().createStatement();
+        boolean result = stat.execute("UPDATE foo SET name = 'FOO'");
+        assertThat(result, is(false));
+
+        // verify intercepted query
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries, hasItem("UPDATE foo SET name = 'FOO'"));
+
+        // verify bar is updated instead of foo
+        ResultSet rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM foo");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("foo"));
+        rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM bar");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("BAR"));
+    }
+
+    @Test
+    public void testExecuteQuery() throws Exception {
+        Statement stat = getProxyConnectionForSelect().createStatement();
+        ResultSet resultSet = stat.executeQuery("SELECT name FROM foo");
+        assertThat(resultSet.next(), is(true));
+        assertThat(resultSet.getInt("id"), is(100));
+        assertThat(resultSet.getString("name"), is("bar"));
+
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries.get(0), is("SELECT name FROM foo"));
+    }
+
+    @Test
+    public void testExecuteUpdate() throws Exception {
+        Statement stat = getProxyConnectionForUpdate().createStatement();
+        int count = stat.executeUpdate("UPDATE foo SET name = 'FOO'");
+        assertThat(count, is(1));
+
+        assertThat(interceptedQueries, hasSize(1));
+        assertThat(interceptedQueries.get(0), is("UPDATE foo SET name = 'FOO'"));
+
+        // verify bar is updated instead of foo
+        ResultSet rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM foo");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("foo"));
+        rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM bar");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("BAR"));
+    }
+
+    @Test
+    public void testExecuteBatch() throws Exception {
+        Statement stat = getProxyConnectionForUpdate().createStatement();
+        stat.addBatch("UPDATE foo SET name = 'FOO1'");
+        stat.addBatch("UPDATE foo SET name = 'FOO2'");
+        int[] result = stat.executeBatch();
+        assertThat(result.length, is(2));
+        assertThat(result[0], is(1));
+        assertThat(result[1], is(1));
+
+        assertThat(interceptedQueries, hasSize(2));
+        assertThat(interceptedQueries.get(0), is("UPDATE foo SET name = 'FOO1'"));
+        assertThat(interceptedQueries.get(1), is("UPDATE foo SET name = 'FOO2'"));
+
+        // verify bar is updated instead of foo
+        ResultSet rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM foo");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("foo"));
+        rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM bar");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("BAR"));
+    }
+
+    @Test
+    public void testClearBatch() throws Exception {
+        Statement stat = getProxyConnectionForUpdate().createStatement();
+        stat.addBatch("UPDATE foo SET name = 'FOO1'");
+        stat.addBatch("UPDATE foo SET name = 'FOO2'");
+        stat.clearBatch();
+        int[] result = stat.executeBatch();
+        assertThat(result.length, is(0));
+
+        // even though batch is canceled, interceptor should be called
+        assertThat(interceptedQueries, hasSize(2));
+        assertThat(interceptedQueries, hasItems("UPDATE foo SET name = 'FOO1'", "UPDATE foo SET name = 'FOO2'"));
+
+        // verify data should not be changed
+        ResultSet rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM foo");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("foo"));
+        rs = rawDatasource.getConnection().createStatement().executeQuery("SELECT name FROM bar");
+        assertThat(rs.next(), is(true));
+        assertThat(rs.getString("name"), is("bar"));
+    }
+
+}
