f04bfa7d149377d6f4475028bb1763b0b0cff179
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-04-16 17:54:02 -0700

    cleanup unused import

--- src/main/java/net/ttddyy/dsproxy/support/CommonsQueryCountLoggingHandlerInterceptor.java
+++ src/main/java/net/ttddyy/dsproxy/support/CommonsQueryCountLoggingHandlerInterceptor.java
@@ -1,16 +1,8 @@
 package net.ttddyy.dsproxy.support;
 
-import net.ttddyy.dsproxy.QueryCount;
-import net.ttddyy.dsproxy.QueryCountHolder;
 import net.ttddyy.dsproxy.listener.CommonsLogLevel;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Spring {@link org.springframework.web.servlet.HandlerInterceptor} to log the query metrics during a http request
