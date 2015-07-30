b2cf8d2907bd574cd1e3bb624b7a45970606dfdc
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-07-29 23:59:06 -0700

    implement ParameterByIndexHolder, ParameterByNameHolder in CallableExecution

--- datasource-proxy-test/src/main/java/net/ttddyy/dsproxy/test/CallableExecution.java
+++ datasource-proxy-test/src/main/java/net/ttddyy/dsproxy/test/CallableExecution.java
@@ -1,12 +1,20 @@
 package net.ttddyy.dsproxy.test;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Tadaya Tsuyukubo
  * @since 1.4
  */
-public class CallableExecution extends BaseQueryExecution implements QueryHolder {
+public class CallableExecution extends BaseQueryExecution implements QueryHolder, ParameterByIndexHolder, ParameterByNameHolder {
 
     public String query;
+    public Map<String, Object> paramsByName = new LinkedHashMap<String, Object>();
+    public Map<Integer, Object> paramsByIndex = new LinkedHashMap<Integer, Object>();
+
 
     @Override
     public String getQuery() {
@@ -16,4 +24,33 @@ public class CallableExecution extends BaseQueryExecution implements QueryHolder
     public void setQuery(String query) {
         this.query = query;
     }
+
+    public void setParamsByName(Map<String, Object> paramsByName) {
+        this.paramsByName = paramsByName;
+    }
+
+    public void setParamsByIndex(Map<Integer, Object> paramsByIndex) {
+        this.paramsByIndex = paramsByIndex;
+    }
+
+    @Override
+    public Map<String, Object> getParamsByName() {
+        return paramsByName;
+    }
+
+    @Override
+    public Map<Integer, Object> getParamsByIndex() {
+        return paramsByIndex;
+    }
+
+    @Override
+    public List<String> getParamNames() {
+        return new ArrayList<String>(this.paramsByName.keySet());
+    }
+
+    @Override
+    public List<Integer> getParamIndexes() {
+        return new ArrayList<Integer>(this.paramsByIndex.keySet());
+    }
+
 }
--- datasource-proxy-test/src/test/java/net/ttddyy/dsproxy/test/hamcrest/CallableExecutionTest.java
+++ datasource-proxy-test/src/test/java/net/ttddyy/dsproxy/test/hamcrest/CallableExecutionTest.java
@@ -1,10 +1,12 @@
 package net.ttddyy.dsproxy.test.hamcrest;
 
 import net.ttddyy.dsproxy.test.CallableExecution;
-import net.ttddyy.dsproxy.test.PreparedExecution;
 import org.hamcrest.Matcher;
 import org.junit.Test;
 
+import static net.ttddyy.dsproxy.test.hamcrest.ParameterHolderAssertions.paramsByIndex;
+import static net.ttddyy.dsproxy.test.hamcrest.ParameterHolderAssertions.paramsByName;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryHolderAssertions.query;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
@@ -18,26 +20,39 @@ import static org.junit.Assert.assertThat;
  */
 public class CallableExecutionTest {
 
-//    @Test
-//    public void testQuery() {
-//        CallableExecution ce = new CallableExecution();
-//        ce.setQuery("foo");
-//
-//        assertThat(pe, query(is("foo")));
-//        assertThat(pe, query(startsWith("fo")));
-//        assertThat(pe.getQuery(), is("foo"));
-//    }
-//
-//    @Test
-//    public void testParams() {
-//        PreparedExecution pe = new PreparedExecution();
-//        pe.getParams().put("foo", "FOO");
-//        pe.getParams().put("bar", "BAR");
-//        pe.getParams().put("baz", "BAZ");
-//        pe.getParams().put("number", 100);
-//        assertThat(pe, params(hasEntry("bar", (Object) "BAR")));
-//        assertThat(pe, params(hasEntry("number", (Object) 100)));
-//    }
+    @Test
+    public void testQuery() {
+        CallableExecution ce = new CallableExecution();
+        ce.setQuery("foo");
+
+        assertThat(ce, query(is("foo")));
+        assertThat(ce, query(startsWith("fo")));
+        assertThat(ce.getQuery(), is("foo"));
+    }
+
+    @Test
+    public void testParamsByName() {
+        CallableExecution ce = new CallableExecution();
+        ce.getParamsByName().put("foo", "FOO");
+        ce.getParamsByName().put("bar", "BAR");
+        ce.getParamsByName().put("baz", "BAZ");
+        ce.getParamsByName().put("number", 100);
+        assertThat(ce, paramsByName(hasEntry("bar", (Object) "BAR")));
+        assertThat(ce, paramsByName(hasEntry("number", (Object) 100)));
+    }
+
+    @Test
+    public void testParamsByIndex() {
+        CallableExecution ce = new CallableExecution();
+        ce.getParamsByIndex().put(1, "FOO");
+        ce.getParamsByIndex().put(2, "BAR");
+        ce.getParamsByIndex().put(3, "BAZ");
+        ce.getParamsByIndex().put(10, 100);
+        assertThat(ce, paramsByIndex(hasEntry(2, (Object) "BAR")));
+        assertThat(ce, paramsByIndex(hasEntry(10, (Object) 100)));
+    }
+
+    // TODO: implement rest of tests
 //
 //    @Test
 //    public void testParamKeys() {
