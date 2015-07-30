1f48f7c151c218ab36c7f59e048ae7574ad818b0
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-07-29 23:30:39 -0700

    add CallableBatchExecutionEntry

--- datasource-proxy-test/src/main/java/net/ttddyy/dsproxy/test/CallableBatchExecution.java
+++ datasource-proxy-test/src/main/java/net/ttddyy/dsproxy/test/CallableBatchExecution.java
@@ -1,7 +1,9 @@
 package net.ttddyy.dsproxy.test;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Tadaya Tsuyukubo
@@ -9,7 +11,48 @@ import java.util.List;
  */
 public class CallableBatchExecution implements BatchParameterHolder, QueryHolder {
 
-    // TODO: implement CallableBatchExecutionEntry
+    public static class CallableBatchExecutionEntry implements BatchExecutionEntry, ParameterByIndexHolder, ParameterByNameHolder {
+
+        public Map<String, Object> paramsByName = new LinkedHashMap<String, Object>();
+        public Map<Integer, Object> paramsByIndex = new LinkedHashMap<Integer, Object>();
+
+        public void setParamsByName(Map<String, Object> paramsByName) {
+            this.paramsByName = paramsByName;
+        }
+
+        public void setParamsByIndex(Map<Integer, Object> paramsByIndex) {
+            this.paramsByIndex = paramsByIndex;
+        }
+
+        @Override
+        public Map<String, Object> getParamsByName() {
+            return paramsByName;
+        }
+
+        @Override
+        public Map<Integer, Object> getParamsByIndex() {
+            return paramsByIndex;
+        }
+
+        @Override
+        public List<String> getParamNames() {
+            return new ArrayList<String>(this.paramsByName.keySet());
+        }
+
+        @Override
+        public List<Integer> getParamIndexes() {
+            return new ArrayList<Integer>(this.paramsByIndex.keySet());
+        }
+
+        @Override
+        public List<Object> getParamValues() {
+            List<Object> list = new ArrayList<Object>();
+            list.addAll(this.paramsByIndex.values());
+            list.addAll(this.paramsByName.values());
+            return list;
+        }
+
+    }
 
 
     public String query;
