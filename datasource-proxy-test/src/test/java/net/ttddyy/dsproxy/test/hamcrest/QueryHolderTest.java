e7d803e0f56cf32b1f6dc6a37dd77bce7dc4990e
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-08-05 18:06:33 -0700

    add failure message test

--- datasource-proxy-test/src/test/java/net/ttddyy/dsproxy/test/hamcrest/QueryHolderTest.java
+++ datasource-proxy-test/src/test/java/net/ttddyy/dsproxy/test/hamcrest/QueryHolderTest.java
@@ -5,8 +5,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static net.ttddyy.dsproxy.test.hamcrest.QueryHolderAssertions.query;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
+import static org.junit.Assert.fail;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author Tadaya Tsuyukubo
@@ -16,15 +20,24 @@ public class QueryHolderTest {
 
     @Test
     public void testQuery() {
-        QueryHolder queryHolder = new QueryHolder() {
-            @Override
-            public String getQuery() {
-                return "foo";
-            }
-        };
+        QueryHolder queryHolder = mock(QueryHolder.class);
+        given(queryHolder.getQuery()).willReturn("foo");
 
         Assert.assertThat(queryHolder, query(is("foo")));
         Assert.assertThat(queryHolder, query(startsWith("f")));
+    }
+
+    @Test
+    public void testQueryMismatch() {
+        QueryHolder queryHolder = mock(QueryHolder.class);
+        given(queryHolder.getQuery()).willReturn("foo");
+
+        try {
+            Assert.assertThat(queryHolder, query(is("FOO")));
+            fail("assertion should fail");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\nExpected: query is \"FOO\"\n     but: query was \"foo\"");
+        }
     }
 
 }
