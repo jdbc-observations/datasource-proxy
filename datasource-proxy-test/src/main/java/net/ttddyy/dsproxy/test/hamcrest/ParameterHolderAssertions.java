4e620ab8ed0b7271e369884d3db0e6d9f57760c5
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-08-09 00:50:51 -0700

    add more methods

--- datasource-proxy-test/src/main/java/net/ttddyy/dsproxy/test/hamcrest/ParameterHolderAssertions.java
+++ datasource-proxy-test/src/main/java/net/ttddyy/dsproxy/test/hamcrest/ParameterHolderAssertions.java
@@ -26,7 +26,7 @@ public class ParameterHolderAssertions {
         };
     }
 
-    // batch(0, paramsByIndex(hasEntry(1, "foo")))
+    // paramsByIndex(hasEntry(1, "foo"))
     public static Matcher<? super ParameterHolder> paramsByIndex(final Matcher<Map<? extends Integer, ?>> mapMatcher) {
         return new ParameterHolderMatcher.ParameterByIndexMatcher<Map<? extends Integer, ?>>(mapMatcher) {
             @Override
@@ -37,7 +37,7 @@ public class ParameterHolderAssertions {
     }
 
 
-    // batch(0, paramIndexes(hasItem(1), hasItem(2)))
+    // paramIndexes(hasItem(1), hasItem(2))
     public static Matcher<? super ParameterHolder> paramIndexes(Matcher<? super Collection<Integer>> collectionMatcher) {
         return new ParameterHolderMatcher.ParameterByIndexMatcher<Collection<Integer>>(collectionMatcher) {
             @Override
@@ -47,13 +47,13 @@ public class ParameterHolderAssertions {
         };
     }
 
-    // batch(0, paramIndexes(1,2,3))
+    // paramIndexes(1,2,3)
     public static Matcher<? super ParameterHolder> paramIndexes(Integer... indexes) {
         return paramIndexes(Matchers.hasItems(indexes));
     }
 
-    // batch(0, paramNames(hasItem("foo"), hasItem("bar")))
-    public static Matcher<? super ParameterHolder> paramNames(Matcher<? super Collection<String>> collectionMatcher) {
+    // param(hasItem("foo"), hasItem("bar"))
+    public static Matcher<? super ParameterHolder> param(Matcher<? super Collection<String>> collectionMatcher) {
         return new ParameterHolderMatcher.ParameterByNameMatcher<Collection<String>>(collectionMatcher) {
             @Override
             public Collection<String> featureValueOf(ParameterByNameHolder actual) {
@@ -62,8 +62,8 @@ public class ParameterHolderAssertions {
         };
     }
 
-    // batch(0, paramValue("foo", is((Object) "FOO")))
-    public static Matcher<? super ParameterHolder> paramValue(final String name, Matcher<Object> matcher) {
+    // param("foo", is((Object) "FOO"))
+    public static Matcher<? super ParameterHolder> param(final String name, Matcher<Object> matcher) {
         return new ParameterHolderMatcher.ParameterByNameMatcher<Object>(matcher) {
             @Override
             public Object featureValueOf(ParameterByNameHolder actual) {
@@ -82,8 +82,8 @@ public class ParameterHolderAssertions {
         };
     }
 
-    // batch(0, paramValue(1, is((Object) "FOO")))
-    public static Matcher<? super ParameterHolder> paramValue(final Integer index, Matcher<Object> matcher) {
+    // param(1, is((Object) "FOO"))
+    public static Matcher<? super ParameterHolder> param(final Integer index, Matcher<Object> matcher) {
         return new ParameterHolderMatcher.ParameterByIndexMatcher<Object>(matcher) {
             @Override
             public Object featureValueOf(ParameterByIndexHolder actual) {
@@ -102,8 +102,8 @@ public class ParameterHolderAssertions {
         };
     }
 
-    //batch(0, paramValue("foo", String.class, is("FOO"))));
-    public static <T> Matcher<? super ParameterHolder> paramValue(final String key, final Class<T> clazz, Matcher<? super T> matcher) {
+    //param("foo", String.class, is("FOO"))
+    public static <T> Matcher<? super ParameterHolder> param(final String key, final Class<T> clazz, Matcher<? super T> matcher) {
         return new ParameterHolderMatcher.ParameterByNameMatcher<T>(matcher) {
             @Override
             @SuppressWarnings("unchecked")
@@ -128,8 +128,8 @@ public class ParameterHolderAssertions {
         };
     }
 
-    //batch(0, paramValue(1, String.class, is("FOO"))));
-    public static <T> Matcher<? super ParameterHolder> paramValue(final Integer index, final Class<T> clazz, Matcher<? super T> matcher) {
+    //param(1, String.class, is("FOO"))
+    public static <T> Matcher<? super ParameterHolder> param(final Integer index, final Class<T> clazz, Matcher<? super T> matcher) {
         return new ParameterHolderMatcher.ParameterByIndexMatcher<T>(matcher) {
             @Override
             @SuppressWarnings("unchecked")
@@ -154,13 +154,27 @@ public class ParameterHolderAssertions {
         };
     }
 
-    public static Matcher<? super ParameterHolder> paramValueAsString(final Integer index, Matcher<? super String> matcher) {
-        return paramValue(index, String.class, matcher);
+    public static Matcher<? super ParameterHolder> paramAsString(final Integer index, Matcher<? super String> matcher) {
+        return param(index, String.class, matcher);
     }
 
-    public static Matcher<? super ParameterHolder> paramValueAsInteger(final Integer index, Matcher<? super Integer> matcher) {
-        return paramValue(index, Integer.class, matcher);
+    public static Matcher<? super ParameterHolder> paramAsInteger(final Integer index, Matcher<? super Integer> matcher) {
+        return param(index, Integer.class, matcher);
+    }
+    public static Matcher<? super ParameterHolder> paramAsLong(final Integer index, Matcher<? super Long> matcher) {
+        return param(index, Long.class, matcher);
+    }
+    public static Matcher<? super ParameterHolder> paramAsDouble(final Integer index, Matcher<? super Double> matcher) {
+        return param(index, Double.class, matcher);
+    }
+    public static Matcher<? super ParameterHolder> paramAsShort(final Integer index, Matcher<? super Short> matcher) {
+        return param(index, Short.class, matcher);
     }
 
+    // TODO: handle setNull
+    // String, Boolean, Byte, Short, Int, Long, Float, Double, BigDecimal, byte[](Bytes), Date, Time,TimeStamp, (Object),
+    // Array,Blob, Clob, NClob, java.net.URL
+
+    // TODO more impl of paramValuAs...
 
 }
