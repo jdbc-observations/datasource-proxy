b1f3d74bb13d81be2ec2f93ccfab84ccbe4b00ef
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-08-12 13:46:54 -0700

    add query type assertions
    - select, insert, update, delete, other

--- datasource-proxy-test/src/main/java/net/ttddyy/dsproxy/test/hamcrest/QueryExecutionAssertions.java
+++ datasource-proxy-test/src/main/java/net/ttddyy/dsproxy/test/hamcrest/QueryExecutionAssertions.java
@@ -1,5 +1,6 @@
 package net.ttddyy.dsproxy.test.hamcrest;
 
+import net.ttddyy.dsproxy.QueryType;
 import net.ttddyy.dsproxy.test.QueryExecution;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -90,30 +91,60 @@ public class QueryExecutionAssertions {
         return new ExecutionTypeMatcher(ExecutionType.IS_CALLABLE_OR_BATCH_CALLABLE);
     }
 
-    // TODO: impl
     public static Matcher<? super QueryExecution> select() {
-        return null;
+        return new QueryTypeMatcher(QueryType.SELECT);
     }
 
-    // TODO: impl
     public static Matcher<? super QueryExecution> insert() {
-        return null;
+        return new QueryTypeMatcher(QueryType.INSERT);
     }
 
-    // TODO: impl
     public static Matcher<? super QueryExecution> update() {
-        return null;
+        return new QueryTypeMatcher(QueryType.UPDATE);
     }
 
-    // TODO: impl
     public static Matcher<? super QueryExecution> delete() {
-        return null;
+        return new QueryTypeMatcher(QueryType.DELETE);
     }
 
-    // TODO: impl
     public static Matcher<? super QueryExecution> other() {
-        return null;
+        return new QueryTypeMatcher(QueryType.OTHER);
     }
+
+
+    private static class QueryTypeMatcher extends TypeSafeMatcher<QueryExecution> {
+
+        private QueryType expectedQueryType;
+
+        private QueryTypeMatcher(QueryType expectedQueryType) {
+            this.expectedQueryType = expectedQueryType;
+        }
+
+        @Override
+        protected boolean matchesSafely(QueryExecution item) {
+            return this.expectedQueryType.equals(item.getQueryType());
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            // expected clause
+            description.appendText(this.expectedQueryType.name());
+        }
+
+        @Override
+        protected void describeMismatchSafely(QueryExecution item, Description mismatchDescription) {
+            // but was clause
+            mismatchDescription.appendText("was ");
+            QueryType queryType = item.getQueryType();
+            if (queryType == null) {
+                mismatchDescription.appendText(null);
+            } else {
+                mismatchDescription.appendText(queryType.name());
+            }
+        }
+
+    }
+
 
     // TODO: impl
     // alias of batch()
@@ -166,4 +197,6 @@ public class QueryExecutionAssertions {
         return null;
     }
 
+
+    // TODO: isUpdate, isSelect, isDelete, isInsert, isOther
 }
--- datasource-proxy-test/src/test/java/net/ttddyy/dsproxy/test/hamcrest/QueryExecutionAssertionsTest.java
+++ datasource-proxy-test/src/test/java/net/ttddyy/dsproxy/test/hamcrest/QueryExecutionAssertionsTest.java
@@ -1,5 +1,6 @@
 package net.ttddyy.dsproxy.test.hamcrest;
 
+import net.ttddyy.dsproxy.QueryType;
 import net.ttddyy.dsproxy.test.CallableBatchExecution;
 import net.ttddyy.dsproxy.test.CallableExecution;
 import net.ttddyy.dsproxy.test.PreparedBatchExecution;
@@ -8,9 +9,7 @@ import net.ttddyy.dsproxy.test.QueryExecution;
 import net.ttddyy.dsproxy.test.StatementBatchExecution;
 import net.ttddyy.dsproxy.test.StatementExecution;
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.batch;
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.batchCallable;
@@ -18,12 +17,20 @@ import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.batchPre
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.batchStatement;
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.callable;
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.callabledOrBatchCallable;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.delete;
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.failure;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.insert;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.other;
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.prepared;
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.preparedOrBatchPrepared;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.select;
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.statement;
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.statementOrBatchStatement;
 import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.success;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryExecutionAssertions.update;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -32,9 +39,6 @@ import static org.mockito.Mockito.when;
  * @since 1.4
  */
 public class QueryExecutionAssertionsTest {
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void testSuccess() {
@@ -50,9 +54,12 @@ public class QueryExecutionAssertionsTest {
         when(qe.isSuccess()).thenReturn(false);
 
 
-        expectedException.expect(AssertionError.class);
-        expectedException.expectMessage("\nExpected: success\n     but: was failure");
-        Assert.assertThat(qe, success());
+        try {
+            Assert.assertThat(qe, success());
+            fail("assertion should fail");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\nExpected: success\n     but: was failure");
+        }
     }
 
 
@@ -69,9 +76,13 @@ public class QueryExecutionAssertionsTest {
         QueryExecution qe = mock(QueryExecution.class);
         when(qe.isSuccess()).thenReturn(true);
 
-        expectedException.expect(AssertionError.class);
-        expectedException.expectMessage("\nExpected: failure\n     but: was success");
-        Assert.assertThat(qe, failure());
+        try {
+            Assert.assertThat(qe, failure());
+            fail("assertion should fail");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\nExpected: failure\n     but: was success");
+        }
+
     }
 
 
@@ -102,5 +113,39 @@ public class QueryExecutionAssertionsTest {
         Assert.assertThat(sbe, batch());
         Assert.assertThat(pbe, batch());
         Assert.assertThat(cbe, batch());
+    }
+
+    @Test
+    public void testQueryType() {
+        QueryExecution select = mock(QueryExecution.class);
+        QueryExecution insert = mock(QueryExecution.class);
+        QueryExecution update = mock(QueryExecution.class);
+        QueryExecution delete = mock(QueryExecution.class);
+        QueryExecution other = mock(QueryExecution.class);
+        given(select.getQueryType()).willReturn(QueryType.SELECT);
+        given(insert.getQueryType()).willReturn(QueryType.INSERT);
+        given(update.getQueryType()).willReturn(QueryType.UPDATE);
+        given(delete.getQueryType()).willReturn(QueryType.DELETE);
+        given(other.getQueryType()).willReturn(QueryType.OTHER);
+
+        Assert.assertThat(select, select());
+        Assert.assertThat(insert, insert());
+        Assert.assertThat(update, update());
+        Assert.assertThat(delete, delete());
+        Assert.assertThat(other, other());
+    }
+
+    @Test
+    public void testQueryTypeUnmatchedMessage() {
+        QueryExecution select = mock(QueryExecution.class);
+        given(select.getQueryType()).willReturn(QueryType.SELECT);
+
+        try {
+            Assert.assertThat(select, insert());
+            fail("assertion should fail");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\nExpected: INSERT\n     but: was SELECT");
+        }
+
     }
 }
