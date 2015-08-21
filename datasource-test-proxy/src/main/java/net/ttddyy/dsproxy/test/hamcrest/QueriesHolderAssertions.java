423596b2bd2c46fec14b8b624177099b63357015
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-08-21 11:51:40 -0700

    add queryTypes assertion

--- datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/hamcrest/QueriesHolderAssertions.java
+++ datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/hamcrest/QueriesHolderAssertions.java
@@ -1,6 +1,7 @@
 package net.ttddyy.dsproxy.test.hamcrest;
 
 import net.ttddyy.dsproxy.test.QueriesHolder;
+import net.ttddyy.dsproxy.test.QueryHolder;
 import org.hamcrest.Description;
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
@@ -45,6 +46,40 @@ public class QueriesHolderAssertions {
             protected Collection<String> featureValueOf(QueriesHolder actual) {
                 return actual.getQueries();
             }
+        };
+    }
+
+    public static Matcher<? super QueriesHolder> queryTypes(final int index, Matcher<? super QueryHolder> queryHolderMatcher) {
+        return new CompositeMatcher<QueriesHolder, QueryHolder>(queryHolderMatcher) {
+            @Override
+            protected boolean validateByThisMatcher(QueriesHolder item, Description expected, Description actual) {
+                int size = item.getQueries().size();
+                if (size - 1 < index) {
+                    expected.appendText("queries[" + index + "] exists");
+                    actual.appendText("queries[] size was " + size);
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            public QueryHolder getValue(QueriesHolder actual) {
+                // convert query to a placeholder QueryHolder instance
+                final String query = actual.getQueries().get(index);
+                QueryHolder queryHolder = new QueryHolder() {
+                    @Override
+                    public String getQuery() {
+                        return query;
+                    }
+                };
+                return queryHolder;
+            }
+
+            @Override
+            public String getSubMatcherFailureDescriptionPrefix() {
+                return "queries[" + index + "] ";
+            }
+
         };
     }
 
--- datasource-test-proxy/src/test/java/net/ttddyy/dsproxy/test/hamcrest/QueriesHolderAssertionsTest.java
+++ datasource-test-proxy/src/test/java/net/ttddyy/dsproxy/test/hamcrest/QueriesHolderAssertionsTest.java
@@ -5,6 +5,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static net.ttddyy.dsproxy.test.hamcrest.QueriesHolderAssertions.queries;
+import static net.ttddyy.dsproxy.test.hamcrest.QueriesHolderAssertions.queryTypes;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryHolderAssertions.delete;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryHolderAssertions.insert;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryHolderAssertions.select;
+import static net.ttddyy.dsproxy.test.hamcrest.QueryHolderAssertions.update;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
@@ -85,6 +90,47 @@ public class QueriesHolderAssertionsTest {
             Assert.assertThat(sbe, queries(hasItem("FOO")));
         } catch (AssertionError e) {
             assertThat(e).hasMessage("\nExpected: queries[] a collection containing \"FOO\"\n     but: queries[] mismatches were: [was \"foo\"]");
+        }
+    }
+
+
+    @Test
+    public void queryTypesWithIndex() {
+        StatementBatchExecution sbe = new StatementBatchExecution();
+        sbe.getQueries().add("SELECT");
+        sbe.getQueries().add("INSERT");
+        sbe.getQueries().add("UPDATE");
+        sbe.getQueries().add("DELETE");
+
+        Assert.assertThat(sbe, queryTypes(0, select()));
+        Assert.assertThat(sbe, queryTypes(1, insert()));
+        Assert.assertThat(sbe, queryTypes(2, update()));
+        Assert.assertThat(sbe, queryTypes(3, delete()));
+    }
+
+    @Test
+    public void queryTypesWithIndexWhenExpectationMismatch() {
+        StatementBatchExecution sbe = new StatementBatchExecution();
+        sbe.getQueries().add("SELECT");
+
+        try {
+            Assert.assertThat(sbe, queryTypes(0, update()));
+            fail("assertion should fail");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\nExpected: queries[0] query type is \"UPDATE\"\n     but: queries[0] query type was \"SELECT\" (SELECT)");
+        }
+    }
+
+    @Test
+    public void queryTypesWithIndexWhenOutOfIndex() {
+        StatementBatchExecution sbe = new StatementBatchExecution();
+        sbe.getQueries().add("SELECT");
+
+        try {
+            Assert.assertThat(sbe, queryTypes(100, select()));
+            fail("assertion should fail");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\nExpected: queries[100] exists\n     but: queries[] size was 1");
         }
     }
 
