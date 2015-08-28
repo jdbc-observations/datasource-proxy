4ad1c4ff955f78e25623d6ada05cd8a5705f8c0f
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-08-27 17:39:54 -0700

    document QueryExecutionAssertions

--- datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/hamcrest/QueryExecutionAssertions.java
+++ datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/hamcrest/QueryExecutionAssertions.java
@@ -6,11 +6,19 @@ import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
 /**
+ * Hamcrest matchers for {@link QueryExecution}.
+ *
  * @author Tadaya Tsuyukubo
  * @since 1.4
  */
 public class QueryExecutionAssertions {
 
+    /**
+     * Matcher to check {@link QueryExecution} was successful.
+     *
+     * Example:
+     * <pre> assertThat(qe, success()); </pre>
+     */
     public static Matcher<QueryExecution> success() {
         return new TypeSafeMatcher<QueryExecution>() {
             @Override
@@ -30,6 +38,12 @@ public class QueryExecutionAssertions {
         };
     }
 
+    /**
+     * Matcher to check {@link QueryExecution} was failure.
+     *
+     * Example:
+     * <pre> assertThat(qe, failure()); </pre>
+     */
     public static Matcher<QueryExecution> failure() {
         return new TypeSafeMatcher<QueryExecution>() {
             @Override
@@ -49,99 +63,105 @@ public class QueryExecutionAssertions {
         };
     }
 
-
+    /**
+     * Matcher to check {@link QueryExecution} was a batch execution.
+     *
+     * Example:
+     * <pre> assertThat(qe, batch()); </pre>
+     */
     public static Matcher<? super QueryExecution> batch() {
         return new ExecutionTypeMatcher(ExecutionType.IS_BATCH);
     }
 
+
+    /**
+     * Matcher to check {@link QueryExecution} was an execution of {@link java.sql.Statement}.
+     *
+     * Example:
+     * <pre> assertThat(qe, statement()); </pre>
+     */
     public static Matcher<? super QueryExecution> statement() {
         return new ExecutionTypeMatcher(ExecutionType.IS_STATEMENT);
     }
 
+    /**
+     * Matcher to check {@link QueryExecution} was a batch execution of {@link java.sql.Statement}.
+     *
+     * Example:
+     * <pre> assertThat(qe, batchStatement()); </pre>
+     */
     public static Matcher<? super QueryExecution> batchStatement() {
         return new ExecutionTypeMatcher(ExecutionType.IS_BATCH_STATEMENT);
     }
 
+    /**
+     * Matcher to check {@link QueryExecution} was a normal or batch execution of {@link java.sql.Statement}.
+     *
+     * Example:
+     * <pre> assertThat(qe, statementOrBatchStatement()); </pre>
+     */
     public static Matcher<? super QueryExecution> statementOrBatchStatement() {
         return new ExecutionTypeMatcher(ExecutionType.IS_STATEMENT_OR_BATCH_STATEMENT);
     }
 
+    /**
+     * Matcher to check {@link QueryExecution} was an execution of {@link java.sql.PreparedStatement}.
+     *
+     * Example:
+     * <pre> assertThat(qe, prepared()); </pre>
+     */
     public static Matcher<? super QueryExecution> prepared() {
         return new ExecutionTypeMatcher(ExecutionType.IS_PREPARED);
     }
 
+    /**
+     * Matcher to check {@link QueryExecution} was a batch execution of {@link java.sql.PreparedStatement}.
+     *
+     * Example:
+     * <pre> assertThat(qe, batchPrepared()); </pre>
+     */
     public static Matcher<? super QueryExecution> batchPrepared() {
         return new ExecutionTypeMatcher(ExecutionType.IS_BATCH_PREPARED);
     }
 
+    /**
+     * Matcher to check {@link QueryExecution} was a normal or batch execution of {@link java.sql.PreparedStatement}.
+     *
+     * Example:
+     * <pre> assertThat(qe, preparedOrBatchPrepared()); </pre>
+     */
     public static Matcher<? super QueryExecution> preparedOrBatchPrepared() {
         return new ExecutionTypeMatcher(ExecutionType.IS_PREPARED_OR_BATCH_PREPARED);
     }
 
+    /**
+     * Matcher to check {@link QueryExecution} was an execution of {@link java.sql.CallableStatement}.
+     *
+     * Example:
+     * <pre> assertThat(qe, callable()); </pre>
+     */
     public static Matcher<? super QueryExecution> callable() {
         return new ExecutionTypeMatcher(ExecutionType.IS_CALLABLE);
     }
 
+    /**
+     * Matcher to check {@link QueryExecution} was a batch execution of {@link java.sql.CallableStatement}.
+     *
+     * Example:
+     * <pre> assertThat(qe, batchCallable()); </pre>
+     */
     public static Matcher<? super QueryExecution> batchCallable() {
         return new ExecutionTypeMatcher(ExecutionType.IS_BATCH_CALLABLE);
     }
 
+    /**
+     * Matcher to check {@link QueryExecution} was a normal or batch execution of {@link java.sql.CallableStatement}.
+     *
+     * Example:
+     * <pre> assertThat(qe, callableOrBatchCallable()); </pre>
+     */
     public static Matcher<? super QueryExecution> callableOrBatchCallable() {
         return new ExecutionTypeMatcher(ExecutionType.IS_CALLABLE_OR_BATCH_CALLABLE);
     }
 
-
-    // TODO: impl
-    // alias of batch()
-    public static Matcher<? super QueryExecution> isBatch() {
-        return null;
-    }
-
-    // TODO: impl
-    public static Matcher<? super QueryExecution> isStatement() {
-        return null;
-    }
-
-    // TODO: impl
-    public static Matcher<? super QueryExecution> isBatchStatement() {
-        return null;
-    }
-
-    // TODO: impl
-    public static Matcher<? super QueryExecution> isStatementOrBatchStatement() {
-        return null;
-    }
-
-    // TODO: impl
-    public static Matcher<? super QueryExecution> isPrepared() {
-        return null;
-    }
-
-    // TODO: impl
-    public static Matcher<? super QueryExecution> isBatchPrepared() {
-        return null;
-    }
-
-    // TODO: impl
-    public static Matcher<? super QueryExecution> isPreparedOrBatchPrepared() {
-        return null;
-    }
-
-    // TODO: impl
-    public static Matcher<? super QueryExecution> isCallable() {
-        return null;
-    }
-
-    // TODO: impl
-    public static Matcher<? super QueryExecution> isBatchCallable() {
-        return null;
-    }
-
-    // TODO: impl
-    public static Matcher<? super QueryExecution> isCallableOrBatchCallable() {
-        return null;
-    }
-
-
-    // TODO: isUpdate, isSelect, isDelete, isInsert, isOther
 }
