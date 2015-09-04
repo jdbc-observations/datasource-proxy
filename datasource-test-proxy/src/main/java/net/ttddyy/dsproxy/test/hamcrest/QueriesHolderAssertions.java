e57e7f1d73a9efedc50cbbbfc5a7ace4808ce2d6
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-09-04 14:35:53 -0700

    add documentation on QueriesHolderAssertions

--- datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/hamcrest/QueriesHolderAssertions.java
+++ datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/hamcrest/QueriesHolderAssertions.java
@@ -9,11 +9,19 @@ import org.hamcrest.Matcher;
 import java.util.Collection;
 
 /**
+ * Hamcrest matchers for {@link QueriesHolder}.
+ *
  * @author Tadaya Tsuyukubo
  * @since 1.4
  */
 public class QueriesHolderAssertions {
 
+    /**
+     * Matcher to check the specified index query as {@link String}.
+     *
+     * Example:
+     * <pre> assertThat(ds.getFirstBatchStatement(), queries(0, startsWith("INSERT"))); </pre>
+     */
     public static Matcher<? super QueriesHolder> queries(final int index, Matcher<String> stringMatcher) {
         return new CompositeMatcher<QueriesHolder, String>(stringMatcher) {
             @Override
@@ -40,6 +48,12 @@ public class QueriesHolderAssertions {
         };
     }
 
+    /**
+     * Matcher to check the given queries with string collection matcher.
+     *
+     * Example:
+     * <pre> assertThat(ds.getFirstBatchStatement(), queries(hasItems("foo", "bar"))); </pre>
+     */
     public static Matcher<? super QueriesHolder> queries(Matcher<? super Collection<String>> collectionMatcher) {
         return new FeatureMatcher<QueriesHolder, Collection<String>>(collectionMatcher, "queries[]", "queries[]") {
             @Override
@@ -49,6 +63,12 @@ public class QueriesHolderAssertions {
         };
     }
 
+    /**
+     * Matcher to check the specified index query as {@link String}.
+     *
+     * Example:
+     * <pre> assertThat(ds.getFirstBatchStatement(), queryTypes(0, select())); </pre>
+     */
     public static Matcher<? super QueriesHolder> queryTypes(final int index, Matcher<? super QueryHolder> queryHolderMatcher) {
         return new CompositeMatcher<QueriesHolder, QueryHolder>(queryHolderMatcher) {
             @Override
