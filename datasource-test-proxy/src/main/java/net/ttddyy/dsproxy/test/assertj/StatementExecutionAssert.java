6a9dbda7f9d8487cf2cd4655651a39d8c19adcd4
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-12-14 14:33:18 -0800

    implement isSuccess() and isFailure() for StatementExecutionAssert

--- datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/assertj/StatementExecutionAssert.java
+++ datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/assertj/StatementExecutionAssert.java
@@ -2,23 +2,24 @@ package net.ttddyy.dsproxy.test.assertj;
 
 import net.ttddyy.dsproxy.QueryType;
 import net.ttddyy.dsproxy.test.StatementExecution;
-import org.assertj.core.api.AbstractAssert;
 
 /**
  * @author Tadaya Tsuyukubo
  * @since 1.4
  */
-public class StatementExecutionAssert extends AbstractAssert<StatementExecutionAssert, StatementExecution> {
+public class StatementExecutionAssert extends AbstractExecutionAssert<StatementExecutionAssert, StatementExecution> {
 
     public StatementExecutionAssert(StatementExecution actual) {
         super(actual, StatementExecutionAssert.class);
     }
 
     public StatementExecutionAssert isSuccess() {
+        isExecutionSuccess();
         return this;
     }
 
     public StatementExecutionAssert isFailure() {
+        isExecutionFailure();
         return this;
     }
 
