f75b05810080188d43399a59664f6c5a696109dc
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2016-01-05 00:13:20 -0800

    helper assertions for batch executions of prepared and callable

---
+++ datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/assertj/helper/BatchExecutionEntryAsserts.java
@@ -0,0 +1,43 @@
+package net.ttddyy.dsproxy.test.assertj.helper;
+
+import net.ttddyy.dsproxy.test.BatchExecutionEntry;
+import net.ttddyy.dsproxy.test.BatchParameterHolder;
+import net.ttddyy.dsproxy.test.ParameterHolder;
+import org.assertj.core.api.WritableAssertionInfo;
+
+import java.util.List;
+
+/**
+ * @author Tadaya Tsuyukubo
+ * @since 1.4
+ */
+// TODO: better name
+public class BatchExecutionEntryAsserts extends AbstractHelperAsserts {
+
+    public BatchExecutionEntryAsserts(WritableAssertionInfo info) {
+        super(info);
+    }
+
+    public void assertBatchExecutionEntry(BatchParameterHolder batchParameterHolder, int batchIndex, Class<? extends ParameterHolder> batchExecutionEntryClass) {
+        // validate batch index size
+        List<BatchExecutionEntry> batchEntries = batchParameterHolder.getBatchExecutionEntries();
+        int batchSize = batchEntries.size();
+
+        if (batchIndex < 0) {
+            String message = String.format("\nExpecting: batch index <%d> should be greater than equal to <0>", batchIndex);
+            failWithMessage(message);
+        } else if (batchSize <= batchIndex) {
+            String message = String.format("\nExpecting: batch index <%d> is too big for the batch size <%d>", batchIndex, batchSize);
+            failWithMessage(message);
+        }
+
+        // validate batch execution entry type
+        BatchExecutionEntry batchEntry = batchParameterHolder.getBatchExecutionEntries().get(batchIndex);
+        if (!(batchEntry.getClass().isAssignableFrom(batchExecutionEntryClass))) {
+            failWithMessage("\nExpecting: batch entry\n<%s>\nbut was\n<%s>",
+                    batchExecutionEntryClass.getSimpleName(),
+                    batchEntry.getClass().getSimpleName());
+        }
+    }
+
+}
---
+++ datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/assertj/helper/ExecutionParameterAsserts.java
@@ -0,0 +1,202 @@
+package net.ttddyy.dsproxy.test.assertj.helper;
+
+import net.ttddyy.dsproxy.listener.RegisterOutParameterValueConverter;
+import net.ttddyy.dsproxy.listener.SetNullParameterValueConverter;
+import net.ttddyy.dsproxy.proxy.ParameterKey;
+import net.ttddyy.dsproxy.test.OutParameterHolder;
+import net.ttddyy.dsproxy.test.ParameterHolder;
+import net.ttddyy.dsproxy.test.assertj.data.ExecutionParameter;
+import net.ttddyy.dsproxy.test.assertj.data.ExecutionParameters;
+import org.assertj.core.api.WritableAssertionInfo;
+
+import java.sql.SQLType;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * Reusable assertions for <code>{@link net.ttddyy.dsproxy.test.assertj.data.ExecutionParameter}</code>s.
+ *
+ * @author Tadaya Tsuyukubo
+ * @since 1.4
+ */
+// TODO: better name
+public class ExecutionParameterAsserts extends AbstractHelperAsserts {
+
+
+    private SetNullParameterValueConverter setNullValueConverter = new SetNullParameterValueConverter();
+    private RegisterOutParameterValueConverter registerOutParameterValueConverter = new RegisterOutParameterValueConverter();
+
+
+    public ExecutionParameterAsserts(WritableAssertionInfo info) {
+        super(info);
+    }
+
+    public void assertParameterKeys(ParameterHolder entry, ExecutionParameters params, boolean isCallable) {
+        ExecutionParameters.ExecutionParametersType parametersType = params.getType();
+        SortedSet<ParameterKey> expectedParamKeys = new TreeSet<ParameterKey>(params.getParameterKeys());
+
+        SortedSet<ParameterKey> actualParamKeys = new TreeSet<ParameterKey>(entry.getAllParams().keySet());
+
+        // TODO: order for keys. should be 1,2,10,20 instead of 1, 10, 2, 20
+        // validate keys
+        boolean containsAll = actualParamKeys.containsAll(expectedParamKeys);
+        if (!containsAll) {
+
+            SortedSet<ParameterKey> notFoundParameterKeys = new TreeSet<ParameterKey>(expectedParamKeys);
+            notFoundParameterKeys.removeAll(actualParamKeys);
+
+            String actualKeys = getParameterKeysAsString(actualParamKeys);
+            String expectedKeys = getParameterKeysAsString(expectedParamKeys);
+            String notFoundKeys = getParameterKeysAsString(notFoundParameterKeys);
+
+            failWithMessage("%nExpecting: %s parameter keys%n<%s>%nto contain:%n<%s>%nbut could not find:%n<%s>",
+                    isCallable ? "callable" : "prepared", actualKeys, expectedKeys, notFoundKeys);
+        }
+
+        if (ExecutionParameters.ExecutionParametersType.CONTAINS_EXACTLY == parametersType) {
+            boolean isSameSize = actualParamKeys.size() == expectedParamKeys.size();
+            if (!isSameSize) {
+                SortedSet<ParameterKey> missingParameterKeys = new TreeSet<ParameterKey>(expectedParamKeys);
+                missingParameterKeys.removeAll(actualParamKeys);
+
+                SortedSet<ParameterKey> extraParameterKeys = new TreeSet<ParameterKey>(actualParamKeys);
+                extraParameterKeys.removeAll(expectedParamKeys);
+
+                String actualKeys = getParameterKeysAsString(actualParamKeys);
+                String expectedKeys = getParameterKeysAsString(expectedParamKeys);
+                String missingKeys = getParameterKeysAsString(missingParameterKeys);
+                String extraKeys = getParameterKeysAsString(extraParameterKeys);
+
+                failWithMessage("%nExpecting: %s parameter keys%n<%s>%nto be exactly:%n<%s>%nbut missing keys:%n<%s>%nextra keys:%n<%s>",
+                        "callable", actualKeys, expectedKeys, missingKeys, extraKeys);
+            }
+        }
+    }
+
+
+    public <T extends ParameterHolder> void assertExecutionParameters(ExecutionParameters params, T entry) {
+        // validate key-value pairs
+        for (ExecutionParameter param : params.getParameters()) {
+            assertExecutionParameter(param, entry);
+        }
+
+    }
+
+    public <T extends ParameterHolder> void assertExecutionParameter(ExecutionParameter param, T entry) {
+        ParameterKey parameterKey = param.getKey();
+
+        if (param instanceof ExecutionParameter.ParamExecution) {
+            Object expectedValue = ((ExecutionParameter.ParamExecution) param).getValue();
+            validateParameter(parameterKey, expectedValue, entry);
+
+        } else if (param instanceof ExecutionParameter.SetNullParamExecution) {
+            Integer sqlType = ((ExecutionParameter.SetNullParamExecution) param).getSqlType();
+            validateSetNullParameter(parameterKey, sqlType, entry);
+
+        } else if (param instanceof ExecutionParameter.RegisterOutParamExecutionWithIntType && entry instanceof OutParameterHolder) {
+            int sqlType = ((ExecutionParameter.RegisterOutParamExecutionWithIntType) param).getSqlType();
+            validateOutParamParameterWithInt(parameterKey, sqlType, (OutParameterHolder) entry);
+
+        } else if (param instanceof ExecutionParameter.RegisterOutParamExecutionWithSQLType && entry instanceof OutParameterHolder) {
+            SQLType sqlType = ((ExecutionParameter.RegisterOutParamExecutionWithSQLType) param).getSqlType();
+            validateOutParamParameterWithSQLType(parameterKey, sqlType, (OutParameterHolder) entry);
+
+        } else {
+            // TODO: better error message
+            throw new IllegalStateException("wrong parameter type: " + param.getClass().getSimpleName());
+        }
+
+    }
+
+    public <T extends ParameterHolder> void validateParameter(ParameterKey parameterKey, Object expectedValue, T executionEntry) {
+
+        Object actualValue = executionEntry.getParams().get(parameterKey);
+        if (expectedValue != actualValue) {
+            SortedMap<String, Object> sortedParams = getAllParamsForDisplay(executionEntry);
+            String expectedEntry = String.format("%s=%s", parameterKey.getKeyAsString(), expectedValue);
+            failWithMessage("%nExpecting: parameters %n<%s>%nto contain:%n<[%s]>%nbut could not find:%n<[%s]>", sortedParams, expectedEntry, expectedEntry);
+        }
+    }
+
+    public <T extends ParameterHolder> void validateSetNullParameter(ParameterKey parameterKey, Integer sqlType, T executionEntry) {
+        if (sqlType == null) {
+            return;  // don't check if sqlType is null
+        }
+
+        Object actualValue = executionEntry.getSetNullParams().get(parameterKey);
+        if (sqlType != actualValue) {
+            SortedMap<String, Object> sortedParams = getAllParamsForDisplay(executionEntry);
+            String displayValue = setNullValueConverter.getDisplayValue(sqlType);
+            String expectedEntry = String.format("%s=%s", parameterKey.getKeyAsString(), displayValue);
+            failWithMessage("%nExpecting: parameters %n<%s>%nto contain:%n<[%s]>%nbut could not find:%n<[%s]>", sortedParams, expectedEntry, expectedEntry);
+        }
+    }
+
+    // TODO: consider both int and SQLType to be inter-changeable. (probably some value object to work for both)
+    private void validateOutParamParameterWithInt(ParameterKey parameterKey, int sqlType, OutParameterHolder executionEntry) {
+        Object actualValue = executionEntry.getOutParams().get(parameterKey);
+        if (!new Integer(sqlType).equals(actualValue)) {
+            SortedMap<String, Object> sortedParams = getAllParamsForDisplay(executionEntry);
+            String displayValue = registerOutParameterValueConverter.getDisplayValue(sqlType);
+            String expectedEntry = String.format("%s=%s", parameterKey.getKeyAsString(), displayValue);
+            failWithMessage("%nExpecting: parameters %n<%s>%nto contain:%n<[%s]>%nbut could not find:%n<[%s]>", sortedParams, expectedEntry, expectedEntry);
+        }
+    }
+
+    private void validateOutParamParameterWithSQLType(ParameterKey parameterKey, SQLType sqlType, OutParameterHolder executionEntry) {
+        Object actualValue = executionEntry.getOutParams().get(parameterKey);
+        if (sqlType != actualValue) {
+            SortedMap<String, Object> sortedParams = getAllParamsForDisplay(executionEntry);
+            String displayValue = registerOutParameterValueConverter.getDisplayValue(sqlType);
+            String expectedEntry = String.format("%s=%s", parameterKey.getKeyAsString(), displayValue);
+            failWithMessage("%nExpecting: parameters %n<%s>%nto contain:%n<[%s]>%nbut could not find:%n<[%s]>", sortedParams, expectedEntry, expectedEntry);
+        }
+    }
+
+    private <T extends ParameterHolder> SortedMap<String, Object> getAllParamsForDisplay(T entry) {
+
+        // TODO: make it work for both OutParameterHolder and ParameterHolder
+        Set<ParameterKey> nullParamKeys = entry.getSetNullParams().keySet();
+        Set<ParameterKey> outParamKeys = new HashSet<ParameterKey>();
+        if (entry instanceof OutParameterHolder) {
+            outParamKeys.addAll(((OutParameterHolder) entry).getOutParams().keySet());
+        }
+
+
+        SortedMap<String, Object> sorted = new TreeMap<String, Object>();
+        for (Map.Entry<ParameterKey, Object> parameterEntry : entry.getAllParams().entrySet()) {
+            ParameterKey key = parameterEntry.getKey();
+            String valueToDisplay;
+            if (nullParamKeys.contains(key)) {
+                valueToDisplay = this.setNullValueConverter.getDisplayValue((Integer) parameterEntry.getValue());
+            } else if (outParamKeys.contains(key)) {
+                valueToDisplay = this.registerOutParameterValueConverter.getDisplayValue(parameterEntry.getValue());
+            } else {
+                valueToDisplay = String.valueOf(parameterEntry.getValue());
+            }
+            sorted.put(key.getKeyAsString(), valueToDisplay);  // display {key=val, key2=val2}
+        }
+
+        return sorted;
+    }
+
+    private String getParameterKeysAsString(Set<ParameterKey> keys) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (ParameterKey key : keys) {
+            sb.append(key.getKeyAsString());
+            sb.append(", ");
+        }
+        if (!keys.isEmpty()) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+        sb.append(']');
+        return sb.toString();
+    }
+
+}
