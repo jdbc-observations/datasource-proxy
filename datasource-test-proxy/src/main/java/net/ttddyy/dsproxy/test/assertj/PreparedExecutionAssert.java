fc071fbcc3f893b14bf955e9a4e1c59486cf09a4
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2016-01-11 23:46:08 -0800

    Implement PreparedExecutionAssert

--- datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/assertj/PreparedExecutionAssert.java
+++ datasource-test-proxy/src/main/java/net/ttddyy/dsproxy/test/assertj/PreparedExecutionAssert.java
@@ -1,7 +1,20 @@
 package net.ttddyy.dsproxy.test.assertj;
 
+import net.ttddyy.dsproxy.listener.SetNullParameterValueConverter;
+import net.ttddyy.dsproxy.proxy.ParameterKey;
 import net.ttddyy.dsproxy.test.PreparedExecution;
 import net.ttddyy.dsproxy.test.assertj.data.ExecutionParameter;
+import net.ttddyy.dsproxy.test.assertj.data.ExecutionParameters;
+import net.ttddyy.dsproxy.test.assertj.helper.ExecutionParameterAsserts;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * @author Tadaya Tsuyukubo
@@ -9,18 +22,11 @@ import net.ttddyy.dsproxy.test.assertj.data.ExecutionParameter;
  */
 public class PreparedExecutionAssert extends AbstractExecutionAssert<PreparedExecutionAssert, PreparedExecution> {
 
+    private SetNullParameterValueConverter setNullValueConverter = new SetNullParameterValueConverter();
+    private ExecutionParameterAsserts parameterAsserts = new ExecutionParameterAsserts(this.info);
+
     public PreparedExecutionAssert(PreparedExecution actual) {
         super(actual, PreparedExecutionAssert.class);
-    }
-
-    public PreparedExecutionAssert aa() {
-        new ParameterByIndexHolderAssertion(this.actual).foo();
-        return this;
-    }
-
-    public PreparedExecutionAssert bb() {
-//        new QueryHolderAssert(this.actual).foo("abc");
-        return this;
     }
 
     public PreparedExecutionAssert isSuccess() {
@@ -35,40 +41,162 @@ public class PreparedExecutionAssert extends AbstractExecutionAssert<PreparedExe
 
 
     public PreparedExecutionAssert containsParam(int paramIndex, Object value) {
-        // TODO: impl
+        containsParams(ExecutionParameter.param(paramIndex, value));
         return this;
     }
 
 
     public PreparedExecutionAssert containsNullParam(int index, int sqlType) {
-        // TODO: impl
+        containsParams(ExecutionParameter.nullParam(index, sqlType));
         return this;
     }
 
     public PreparedExecutionAssert containsNullParam(int index) {
-        // TODO: impl
+        containsParams(ExecutionParameter.nullParam(index));
         return this;
     }
 
 
     public PreparedExecutionAssert containsParams(ExecutionParameter... params) {
-        // TODO: impl
+        ExecutionParameters executionParameters = ExecutionParameters.containsParams(params);
+        parameterAsserts.assertParameterKeys(this.actual, executionParameters, false);
+        parameterAsserts.assertExecutionParameters(this.actual, executionParameters);
         return this;
     }
 
-    public PreparedExecutionAssert containsParamIndex(int paramIndexe) {
-        // TODO: impl
+    public PreparedExecutionAssert containsParamIndex(int paramIndex) {
+        containsParamIndexes(paramIndex);
         return this;
     }
 
     public PreparedExecutionAssert containsParamIndexes(int... paramIndexes) {
-        // TODO: impl
+        ExecutionParameters executionParameters = ExecutionParameters.containsParamIndexes(paramIndexes);
+        parameterAsserts.assertParameterKeys(this.actual, executionParameters, false);
         return this;
     }
 
+    /**
+     * @param paramValues Expecting parameter values. setNull is represented as {@code null}.
+     */
     public PreparedExecutionAssert containsParamValuesExactly(Object... paramValues) {
-        // TODO: impl
+        // TODO: cleanup
+
+        // actual param keys are index numbers
+        SortedSet<ParameterKey> keys = new TreeSet<ParameterKey>();
+        keys.addAll(this.actual.getParams().keySet());
+        keys.addAll(this.actual.getSetNullParams().keySet());
+
+        // construct map for expected
+        Map<ParameterKey, Object> expectedParamAndValue = new LinkedHashMap<ParameterKey, Object>();
+        for (int i = 0; i < paramValues.length; i++) {
+            expectedParamAndValue.put(new ParameterKey(i + 1), paramValues[i]);
+        }
+
+        Map<ParameterKey, Object> actualParamAndValue = this.actual.getAllParams();
+        List<Object> actualValues = new ArrayList<Object>();
+        for (ParameterKey key : keys) {
+            Object value;
+            if (actual.getSetNullParams().containsKey(key)) {
+                value = null;  // for setNull, use as null
+            } else {
+                value = actualParamAndValue.get(key);
+            }
+            actualValues.add(value);
+        }
+
+        // size check
+        if (keys.size() != paramValues.length) {
+
+            String failureMessage = getFailureMessageForValuesExactly(keys, actualParamAndValue, expectedParamAndValue);
+            failWithMessage(failureMessage);
+        }
+
+
+        // value check
+        for (int i = 0; i < keys.size(); i++) {
+            Object actualValue = actualValues.get(i);
+            Object expectedValue = paramValues[i];
+
+            if ((actualValue == expectedValue) || (actualValue != null && actualValue.equals(expectedValue))) {
+                continue;
+            }
+
+            String failureMessage = getFailureMessageForValuesExactly(keys, actualParamAndValue, expectedParamAndValue);
+            failWithMessage(failureMessage);
+        }
+
         return this;
+    }
+
+    private String getFailureMessageForValuesExactly(SortedSet<ParameterKey> keys, Map<ParameterKey, Object> actualParamValues, Map<ParameterKey, Object> expectedParamValues) {
+        Set<Integer> actualSetNullParamIndexes = new HashSet<Integer>();
+        actualSetNullParamIndexes.addAll(this.actual.getSetNullParamsByIndex().keySet());
+
+        // getting actual values to display
+        Map<Integer, Object> actualValueMapToDisplay = new LinkedHashMap<Integer, Object>();
+        for (ParameterKey key : keys) {
+            Object value;
+            if (actual.getSetNullParams().containsKey(key)) {
+                Integer sqlType = actual.getSetNullParams().get(key);
+                value = setNullValueConverter.getDisplayValue(sqlType);
+            } else {
+                value = actualParamValues.get(key);
+            }
+            actualValueMapToDisplay.put(key.getIndex(), value);
+        }
+
+        // getting expected values for display
+        Map<Integer, Object> expectedValueMapToDisplay = new LinkedHashMap<Integer, Object>();
+        for (Map.Entry<ParameterKey, Object> entry : expectedParamValues.entrySet()) {
+            expectedValueMapToDisplay.put(entry.getKey().getIndex(), entry.getValue() == null ? "NULL" : entry.getValue());
+        }
+
+
+        String missingMessage = getMissingKeyValueMessage(expectedParamValues, this.actual.getAllParams(), actualSetNullParamIndexes);
+        String extraMessage = getExtraKeyValueMessage(expectedParamValues, this.actual.getAllParams(), actualSetNullParamIndexes);
+
+        return String.format("%nExpecting: prepared parameter values%n<%s>%nto be exactly:%n<%s>%nbut missing:%n<%s>%nextra:%n<%s>",
+                actualValueMapToDisplay, expectedValueMapToDisplay, missingMessage, extraMessage);
+
+    }
+
+
+    private String getMissingKeyValueMessage(Map<ParameterKey, Object> expected, Map<ParameterKey, Object> actual, Set<Integer> actualSetNullParamKeys) {
+        Map<Integer, Object> missing = new LinkedHashMap<Integer, Object>();
+
+        for (Map.Entry<ParameterKey, Object> entry : expected.entrySet()) {
+            int index = entry.getKey().getIndex();
+            Object expectedValue = entry.getValue();
+            Object actualValue = actual.get(new ParameterKey(index));
+            if (expectedValue == null) {
+                if (!actualSetNullParamKeys.contains(index)) {
+                    // expected null, but actual is not null
+                    missing.put(index, "NULL");
+                }
+            } else {
+                if (!expectedValue.equals(actualValue)) {
+                    missing.put(index, expectedValue);  // expected has value but doesn't match
+                }
+            }
+        }
+
+        return missing.toString();
+    }
+
+
+    private String getExtraKeyValueMessage(Map<ParameterKey, Object> expected, Map<ParameterKey, Object> actual, Set<Integer> actualSetNullParamKeys) {
+
+        Map<Integer, Object> extra = new LinkedHashMap<Integer, Object>();
+        for (Map.Entry<ParameterKey, Object> entry : actual.entrySet()) {
+            ParameterKey actualKey = entry.getKey();
+
+            if (!expected.containsKey(actualKey)) {  // map may contains value=null, thus cannot compare the result of get with null
+                // expected doesn't have the index.
+                Object value = actualSetNullParamKeys.contains(actualKey.getIndex()) ? setNullValueConverter.getDisplayValue((Integer) entry.getValue()) : entry.getValue();
+                extra.put(actualKey.getIndex(), value);
+            }
+        }
+        return extra.toString();
     }
 
 }
--- datasource-test-proxy/src/test/java/net/ttddyy/dsproxy/test/assertj/PreparedExecutionAssertTest.java
+++ datasource-test-proxy/src/test/java/net/ttddyy/dsproxy/test/assertj/PreparedExecutionAssertTest.java
@@ -1,9 +1,15 @@
 package net.ttddyy.dsproxy.test.assertj;
 
+import net.ttddyy.dsproxy.proxy.ParameterKey;
+import net.ttddyy.dsproxy.test.CallableExecution;
 import net.ttddyy.dsproxy.test.PreparedExecution;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Types;
+
+import static net.ttddyy.dsproxy.test.assertj.data.ExecutionParameter.nullParam;
+import static net.ttddyy.dsproxy.test.assertj.data.ExecutionParameter.param;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
 
@@ -24,8 +30,6 @@ public class PreparedExecutionAssertTest {
 
     @Test
     public void testIsSuccess() {
-        PreparedExecution ce = new PreparedExecution();
-
         // success case
         this.pe.setSuccess(true);
         this.peAssert.isSuccess();
@@ -55,6 +59,315 @@ public class PreparedExecutionAssertTest {
         } catch (AssertionError e) {
             assertThat(e).hasMessage("\nExpecting: <Failure execution> but was: <Successful execution>\n");
         }
+    }
+
+    @Test
+    public void testContainsParams() {
+        PreparedExecution pe = new PreparedExecution();
+        pe.getParams().put(new ParameterKey(2), "bar");
+        pe.getParams().put(new ParameterKey(1), "foo");
+        pe.getSetNullParams().put(new ParameterKey(3), Types.BIT);
+
+        // successful call
+        DataSourceProxyAssertions.assertThat(pe).containsParams(param(1, "foo"), param(2, "bar"), nullParam(3, Types.BIT));
+        DataSourceProxyAssertions.assertThat(pe).containsParams(param(1, "foo"));
+        DataSourceProxyAssertions.assertThat(pe).containsParams(nullParam(3));
+        DataSourceProxyAssertions.assertThat(pe).containsParams(nullParam(3, Types.BIT));
+
+        // value is wrong for param index
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParams(param(1, "WRONG"));
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: parameters \n" +
+                    "<{1=foo, 2=bar, 3=NULL(BIT)}>\n" +
+                    "to contain:\n" +
+                    "<[1=WRONG]>\n" +
+                    "but could not find:\n" +
+                    "<[1=WRONG]>");
+        }
+
+        // value is wrong for set-null-param index
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParams(nullParam(3, Types.BOOLEAN));
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: parameters \n" +
+                    "<{1=foo, 2=bar, 3=NULL(BIT)}>\n" +
+                    "to contain:\n" +
+                    "<[3=NULL(BOOLEAN)]>\n" +
+                    "but could not find:\n" +
+                    "<[3=NULL(BOOLEAN)]>");
+        }
+
+        // specifying set-null for normal params
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParams(nullParam(2));
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter keys\n" +
+                    "<[1, 2, 3]>\n" +
+                    "(params=[1, 2], set-null=[3], register-out=[])\n" +
+                    "to contain:\n" +
+                    "<set-null=[2]>\n" +
+                    "but could not find:\n" +
+                    "<set-null=[2]>");
+        }
+
+        // no param index key
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParams(param(100, "bar"));
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter keys\n" +
+                    "<[1, 2, 3]>\n" +
+                    "(params=[1, 2], set-null=[3], register-out=[])\n" +
+                    "to contain:\n" +
+                    "<params=[100]>\n" +
+                    "but could not find:\n" +
+                    "<params=[100]>");
+        }
 
     }
+
+
+    @Test
+    public void testContainsNullParam() {
+        PreparedExecution pe = new PreparedExecution();
+        pe.getParams().put(new ParameterKey(1), "foo");
+        pe.getSetNullParams().put(new ParameterKey(2), Types.VARCHAR);
+
+        // successful call
+        DataSourceProxyAssertions.assertThat(pe)
+                .containsNullParam(2)
+                .containsNullParam(2, Types.VARCHAR)
+        ;
+
+        // index with wrong value
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsNullParam(2, Types.BIT);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: parameters \n" +
+                    "<{1=foo, 2=NULL(VARCHAR)}>\n" +
+                    "to contain:\n" +
+                    "<[2=NULL(BIT)]>\n" +
+                    "but could not find:\n" +
+                    "<[2=NULL(BIT)]>");
+        }
+
+        // no index and value
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsNullParam(100, Types.BIT);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter keys\n" +
+                    "<[1, 2]>\n" +
+                    "(params=[1], set-null=[2], register-out=[])\n" +
+                    "to contain:\n" +
+                    "<set-null=[100]>\n" +
+                    "but could not find:\n" +
+                    "<set-null=[100]>");
+        }
+
+        // no index with no value
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsNullParam(100);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter keys\n" +
+                    "<[1, 2]>\n" +
+                    "(params=[1], set-null=[2], register-out=[])\n" +
+                    "to contain:\n" +
+                    "<set-null=[100]>\n" +
+                    "but could not find:\n" +
+                    "<set-null=[100]>");
+        }
+
+        // specifying index for param (not for set-null)
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsNullParam(1);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter keys\n" +
+                    "<[1, 2]>\n" +
+                    "(params=[1], set-null=[2], register-out=[])\n" +
+                    "to contain:\n" +
+                    "<set-null=[1]>\n" +
+                    "but could not find:\n" +
+                    "<set-null=[1]>");
+        }
+
+        // specifying index in out-param
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsNullParam(3, Types.BOOLEAN);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter keys\n" +
+                    "<[1, 2]>\n" +
+                    "(params=[1], set-null=[2], register-out=[])\n" +
+                    "to contain:\n" +
+                    "<set-null=[3]>\n" +
+                    "but could not find:\n" +
+                    "<set-null=[3]>");
+        }
+
+    }
+
+    @Test
+    public void testParamIndex() {
+        PreparedExecution pe = new PreparedExecution();
+        pe.getParams().put(new ParameterKey(1), "foo");
+        pe.getSetNullParams().put(new ParameterKey(2), Types.VARCHAR);
+
+
+        // successful case
+        DataSourceProxyAssertions.assertThat(pe).containsParamIndex(1);
+        DataSourceProxyAssertions.assertThat(pe).containsParamIndex(2);
+
+        // missing param key (index)
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParamIndex(100);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter keys\n" +
+                    "<[1, 2]>\n" +
+                    "to contain:\n" +
+                    "<[100]>\n" +
+                    "but could not find:\n" +
+                    "<[100]>");
+        }
+
+    }
+
+    @Test
+    public void testParamIndexes() {
+        PreparedExecution pe = new PreparedExecution();
+        pe.getParams().put(new ParameterKey(1), "foo");
+        pe.getSetNullParams().put(new ParameterKey(2), Types.VARCHAR);
+
+
+        // successful case
+        DataSourceProxyAssertions.assertThat(pe).containsParamIndexes(1);
+        DataSourceProxyAssertions.assertThat(pe).containsParamIndexes(1, 2);
+
+        // missing one param key (index)
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParamIndexes(1, 2, 100);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter keys\n" +
+                    "<[1, 2]>\n" +
+                    "to contain:\n" +
+                    "<[1, 2, 100]>\n" +
+                    "but could not find:\n" +
+                    "<[100]>");
+        }
+
+    }
+
+    @Test
+    public void testContainsParamValuesExactly() {
+        PreparedExecution pe = new PreparedExecution();
+        pe.getParams().put(new ParameterKey(1), "foo");
+        pe.getParams().put(new ParameterKey(2), 100);
+        pe.getSetNullParams().put(new ParameterKey(3), Types.VARCHAR);
+
+
+        // successful case
+        DataSourceProxyAssertions.assertThat(pe).containsParamValuesExactly("foo", 100, null);
+
+        // extra values
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParamValuesExactly("foo", 100);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter values\n" +
+                    "<{1=foo, 2=100, 3=NULL(VARCHAR)}>\n" +
+                    "to be exactly:\n" +
+                    "<{1=foo, 2=100}>\n" +
+                    "but missing:\n" +
+                    "<{}>\n" +
+                    "extra:\n" +
+                    "<{3=NULL(VARCHAR)}>");
+        }
+
+        // missing values
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParamValuesExactly("foo", 100, null, "bar", "baz");
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter values\n" +
+                    "<{1=foo, 2=100, 3=NULL(VARCHAR)}>\n" +
+                    "to be exactly:\n" +
+                    "<{1=foo, 2=100, 3=NULL, 4=bar, 5=baz}>\n" +
+                    "but missing:\n" +
+                    "<{4=bar, 5=baz}>\n" +
+                    "extra:\n" +
+                    "<{}>");
+        }
+
+        // missing and extra values
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParamValuesExactly("foo", 100, "baz");
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter values\n" +
+                    "<{1=foo, 2=100, 3=NULL(VARCHAR)}>\n" +
+                    "to be exactly:\n" +
+                    "<{1=foo, 2=100, 3=baz}>\n" +
+                    "but missing:\n" +
+                    "<{3=baz}>\n" +
+                    "extra:\n" +
+                    "<{}>");
+        }
+
+
+        // expecting not-null but null
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParamValuesExactly("foo", null, null);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter values\n" +
+                    "<{1=foo, 2=100, 3=NULL(VARCHAR)}>\n" +
+                    "to be exactly:\n" +
+                    "<{1=foo, 2=NULL, 3=NULL}>\n" +
+                    "but missing:\n" +
+                    "<{2=NULL}>\n" +
+                    "extra:\n" +
+                    "<{}>");
+        }
+        // expecting null but not-null
+        try {
+            DataSourceProxyAssertions.assertThat(pe).containsParamValuesExactly("foo", 100, 200);
+            fail("exception should be thrown");
+        } catch (AssertionError e) {
+            assertThat(e).hasMessage("\n" +
+                    "Expecting: prepared parameter values\n" +
+                    "<{1=foo, 2=100, 3=NULL(VARCHAR)}>\n" +
+                    "to be exactly:\n" +
+                    "<{1=foo, 2=100, 3=200}>\n" +
+                    "but missing:\n" +
+                    "<{3=200}>\n" +
+                    "extra:\n" +
+                    "<{}>");
+        }
+
+    }
+
 }
