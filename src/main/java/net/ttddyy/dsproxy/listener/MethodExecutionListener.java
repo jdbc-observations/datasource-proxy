927dbe2ac96bd5c49b0087a867c52467a3465582
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2017-09-06 16:53:20 -0700

    Use NoOpMethodExecutionListener as default

--- src/main/java/net/ttddyy/dsproxy/listener/MethodExecutionListener.java
+++ src/main/java/net/ttddyy/dsproxy/listener/MethodExecutionListener.java
@@ -3,21 +3,14 @@ package net.ttddyy.dsproxy.listener;
 import java.lang.reflect.Method;
 
 /**
+ * Callback listener for JDBC API method invocations.
+ *
  * @author Tadaya Tsuyukubo
  * @since 1.4.3
  */
 public interface MethodExecutionListener {
-    MethodExecutionListener DEFAULT = new MethodExecutionListener() {
-        @Override
-        public void beforeMethod(Object target, Method method, Object[] args) {
-            // no-op
-        }
 
-        @Override
-        public void afterMethod(Object target, Method method, Object[] args, Object result, Throwable thrown, long elapsedTime) {
-            // no-op
-        }
-    };
+    MethodExecutionListener DEFAULT = new NoOpMethodExecutionListener();
 
     void beforeMethod(Object target, Method method, Object[] args);
 
--- src/main/java/net/ttddyy/dsproxy/listener/NoOpMethodExecutionListener.java
+++ src/main/java/net/ttddyy/dsproxy/listener/NoOpMethodExecutionListener.java
@@ -1,7 +1,23 @@
 package net.ttddyy.dsproxy.listener;
 
+import java.lang.reflect.Method;
+
 /**
+ * No-op implementation of {@link MethodExecutionListener}
+ *
  * @author Tadaya Tsuyukubo
+ * @since 1.4.3
  */
-public class NoOpMethodExecutionListener {
+public class NoOpMethodExecutionListener implements MethodExecutionListener {
+
+    @Override
+    public void beforeMethod(Object target, Method method, Object[] args) {
+        // no-op
+    }
+
+    @Override
+    public void afterMethod(Object target, Method method, Object[] args, Object result, Throwable thrown, long elapsedTime) {
+        // no-op
+    }
+
 }
