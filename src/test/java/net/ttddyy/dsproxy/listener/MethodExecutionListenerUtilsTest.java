246b75e440350a1f8caa2c62acc0f4888fa5f412
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2017-09-15 19:59:50 -0700

    Add test for MethodExecutionListenerUtils

---
+++ src/test/java/net/ttddyy/dsproxy/listener/MethodExecutionListenerUtilsTest.java
@@ -0,0 +1,125 @@
+package net.ttddyy.dsproxy.listener;
+
+import net.ttddyy.dsproxy.proxy.ProxyConfig;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.sql.Statement;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Tadaya Tsuyukubo
+ */
+public class MethodExecutionListenerUtilsTest {
+
+    @Test
+    public void invokeNormal() throws Throwable {
+
+        final Object target = new Object();
+        final Method method = Statement.class.getMethod("getConnection");
+        final Object[] methodArgs = new Object[]{};
+        final Object returnObj = new Object();
+
+        CallCheckMethodExecutionListener listener = new CallCheckMethodExecutionListener() {
+            @Override
+            public void beforeMethod(MethodExecutionContext executionContext) {
+                super.beforeMethod(executionContext);
+
+                assertThat(executionContext).isNotNull();
+                assertThat(executionContext.getMethod()).isSameAs(method);
+                assertThat(executionContext.getMethodArgs()).isSameAs(methodArgs);
+                assertThat(executionContext.getTarget()).isSameAs(target);
+                assertThat(executionContext.getElapsedTime()).isEqualTo(0);
+                assertThat(executionContext.getThrown()).isNull();
+                assertThat(executionContext.getResult()).isNull();
+
+                assertThat(executionContext.getProxyConfig()).isNotNull();
+            }
+
+            @Override
+            public void afterMethod(MethodExecutionContext executionContext) {
+                super.afterMethod(executionContext);
+
+                assertThat(executionContext).isNotNull();
+                assertThat(executionContext.getMethod()).isSameAs(method);
+                assertThat(executionContext.getMethodArgs()).isSameAs(methodArgs);
+                assertThat(executionContext.getTarget()).isSameAs(target);
+                assertThat(executionContext.getElapsedTime()).isGreaterThanOrEqualTo(0);
+                assertThat(executionContext.getThrown()).isNull();
+                assertThat(executionContext.getResult()).isSameAs(returnObj);
+
+                assertThat(executionContext.getProxyConfig()).isNotNull();
+            }
+        };
+
+
+        ProxyConfig proxyConfig = ProxyConfig.Builder.create().methodListener(listener).build();
+
+        Object result = MethodExecutionListenerUtils.invoke(new MethodExecutionListenerUtils.MethodExecutionCallback() {
+            @Override
+            public Object execute(Object proxyTarget, Method method, Object[] args) throws Throwable {
+                return returnObj;
+            }
+        }, proxyConfig, target, method, methodArgs);
+
+        assertSame(returnObj, result);
+        assertTrue(listener.isBeforeMethodCalled());
+        assertTrue(listener.isAfterMethodCalled());
+
+        MethodExecutionContext beforeMethodContext = listener.getBeforeMethodContext();
+        MethodExecutionContext afterMethodContext = listener.getAfterMethodContext();
+
+        assertSame("each method should be passed same context object", beforeMethodContext, afterMethodContext);
+        assertThat(beforeMethodContext.getProxyConfig()).isSameAs(proxyConfig);
+    }
+
+    @Test
+    public void invokeWithException() throws Throwable {
+
+        final Object target = new Object();
+        final Method method = Statement.class.getMethod("getConnection");
+        final Object[] methodArgs = new Object[]{};
+        final Exception exception = new Exception();
+
+        CallCheckMethodExecutionListener listener = new CallCheckMethodExecutionListener() {
+            @Override
+            public void beforeMethod(MethodExecutionContext executionContext) {
+                super.beforeMethod(executionContext);
+
+                assertThat(executionContext.getThrown()).isNull();
+                assertThat(executionContext.getResult()).isNull();
+            }
+
+            @Override
+            public void afterMethod(MethodExecutionContext executionContext) {
+                super.afterMethod(executionContext);
+
+                assertThat(executionContext.getThrown()).isSameAs(exception);
+            }
+        };
+
+
+        ProxyConfig proxyConfig = ProxyConfig.Builder.create().methodListener(listener).build();
+
+        // when callback throws exception
+        Throwable thrownException = null;
+        try {
+            MethodExecutionListenerUtils.invoke(new MethodExecutionListenerUtils.MethodExecutionCallback() {
+                @Override
+                public Object execute(Object proxyTarget, Method method, Object[] args) throws Throwable {
+                    throw exception;
+                }
+            }, proxyConfig, target, method, methodArgs);
+        } catch (Throwable throwable) {
+            thrownException = throwable;
+        }
+
+        assertSame(exception, thrownException);
+        assertTrue(listener.isBeforeMethodCalled());
+        assertTrue(listener.isAfterMethodCalled());
+    }
+
+}
