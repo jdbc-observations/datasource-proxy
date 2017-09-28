c6da4062fe0627dae0da54dc0a00bcbe91b2f960
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2017-09-28 11:31:31 -0700

    Publish docs to snapshot directory

--- src/build/publish-doc.sh
+++ src/build/publish-doc.sh
@@ -2,6 +2,7 @@
 set -e # exit with nonzero exit code if anything fails
 
 if [ "$TRAVIS_BRANCH" != 'master' ] || [ "$TRAVIS_PULL_REQUEST" == 'true' ]; then
+    echo "Not publishing documents. branch=\"${TRAVIS_BRANCH}\" PR=\"${TRAVIS_PULL_REQUEST}\""
     exit 0
 fi
 
@@ -13,7 +14,7 @@ git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ttddyy/datasource-proxy gh-pages > /dev/null
 
 cd gh-pages
-cp -Rf $TRAVIS_BUILD_DIR/target/generated-docs/index.html docs/current/user-guide/
+cp -Rf $TRAVIS_BUILD_DIR/target/generated-docs/index.html docs/snapshot/user-guide/
 git add -f .
 git commit -m "Latest documentation on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
