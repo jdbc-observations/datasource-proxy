e1cc5b29b8d6c742aaff523bfc5db35fb620ff34
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2017-10-13 20:43:29 -0700

    Publish snapshot javadoc

--- src/build/publish-doc.sh
+++ src/build/publish-doc.sh
@@ -6,6 +6,8 @@ if [ "$TRAVIS_BRANCH" != 'master' ] || [ "$TRAVIS_PULL_REQUEST" == 'true' ]; the
     exit 0
 fi
 
+./mvnw -Ppublish-doc asciidoctor:process-asciidoc@output-html javadoc:javadoc
+
 cd $HOME
 rm -fr gh-pages
 
@@ -14,7 +16,10 @@ git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ttddyy/datasource-proxy gh-pages > /dev/null
 
 cd gh-pages
+mkdir -p docs/snapshot/user-guide/
+mkdir -p docs/snapshot/api/
 cp -Rf $TRAVIS_BUILD_DIR/target/generated-docs/index.html docs/snapshot/user-guide/
+cp -Rf $TRAVIS_BUILD_DIR/target/site/apidocs/* docs/snapshot/api/
 git add -f .
 git commit -m "Latest documentation on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
